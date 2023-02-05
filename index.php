9596fcfa25beb276eefaa1a600dccbf9f119b3a3
Anjali Bohra <anjali.bohra2111@gmail.com>
2023-02-05 18:56:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,173 +1,198 @@
-<?php
-$insert = false;
-
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    include 'utilities\_dbconnect.php';
-    $pname = $_POST["name"];
-    $email = $_POST["email"];
-    $contact = $_POST["contact"];
-    $address = $_POST["address"];
-    $city= $_POST["city"];
-    $state= $_POST["state"];
-    $zip= $_POST["zip"];
-    $femail= $_POST["femail"];
-    $webaddress= $_POST["webadd"];
-    $list= $_POST["list"];
-    $description= $_POST["description"];
-
-    //sql query
-    $sql = "INSERT INTO `lawyers` ( `full-name`, `email`, `contact`, `address`, `city`, `state`, `zipcode`) VALUES ('$pname','$email','$contact','$address','$city','$state','$zip');"; 
-   $ssql = "INSERT INTO `firms` (`date`, `email`, `web-address`, `category`, `description`) VALUES (current_timestamp(), '$femail', '$webaddress', '$list', '$description');";
-    // $mysqli->multi_query($sql);
-    // do {
-    //     if ($result = $mysqli->store_result()) {
-    //         var_dump($result->fetch_all(MYSQLI_ASSOC));
-    //         $result->free();
-    //     }
-    // } while ($mysqli->next_result());
-    $result = mysqli_query($conn, $sql, $ssql);
-    //checking
-    if($result){
-        $insert = true;
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="lawyer.css">
-    <title>Lawyer-form</title>
+  <title>HOMEPAGE</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/c85d084fd0.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
-        <!-- NAV-BAR -->
-        <?php require 'utilities\navbar.php' ?> 
-    <?php
-    if($insert){
-        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-        <strong>Success!</strong> 
-        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-          <span aria-hidden='true'>&times;</span>
-        </button>
-      </div>";
-    }
-    ?>
-    <div class="container-contact1">
-        <div class="container1">
-            <div class="heading"><b>Register Here</b> </div>
-            <!-- <div class="container"> -->
-            <form class="contact1-form" action="lawyer.php" method="post">
-                <div class="card-details">
-                    <p class="a1 " style="text-align: justify;">
-                        Volunteering is an excellent way to broaden your social support network and keep your mind
-                        active by learning new things. It is an easy way to build connections with your local community;
-                        it also gives you an opportunity to share your knowledge and experiences, make a positive
-                        difference, and feel good about yourself.
-                    </p>
-                    <div class="a2"><b>If you want voluntarily sign-up and provide legal support/advice to our
-                        victims </b></div>
+  <!-- navbar  -->
+  <?php require 'utilities\navbar.php' ?> 
+  <br>
+  <!-- page1 -->
+  <section class="conatiner px-0 mt-5" id="help7">
+    <div class="row align-items-center" id="help6">
 
-                    <div class="mt-4">
-                        <h4><b>About Yourself: </b> </h4>
-                        <div class="input-group card-box">
-                            <p style="font-size: 19px;" name="y/n">Are you comfortable initially communicating with a client by answering a question via email ?</p>
-                            <div class="form-check form-check-inline" id="nnnn">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
-                                    value="option1">
-                                <label class="form-check-label" for="inlineRadio1">YES</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                                    value="option2">
-                                <label class="form-check-label" for="inlineRadio2">NO</label>
-                            </div>
-                        </div>
-
-                        <div class="input-group card-box">
-
-                            <span class="details">Full Name</span>
-                            <input type="text" name="name" placeholder="Enter your name">
-                        </div>
-                        <div class="input-group card-box">
-                            <span class="details">Email</span>
-                            <input type="email" name="email" placeholder="Email">
-                        </div>
-                        <div class="input-group card-box">
-                            <span class="details">Contact</span>
-                            <input type="number" name="contact" placeholder="Number">
-                        </div>
-                        <div class="input-group card-box">
-                            <span class="details">Address</span>
-                            <textarea id="address" name="address" rows="8" cols="80" placeholder="Address"></textarea>
-                        </div>
-
-                        <div class="input-group card-box">
-                            <span class="input-group-addon details">City</span>
-                            <input id="msg" type="text" class="form-control" name="city" placeholder="City">
-                        </div>
-
-                        <div class="input-group card-box">
-                            <span class="input-group-addon details">State</span>
-                            <input id="msg" type="text" class="form-control" name="state" placeholder="State">
-                        </div>
-
-
-
-                        <div class="input-group card-box">
-                            <span class="input-group-addon details">Zipcode</span>
-                            <input id="msg" type="text" class="form-control" name="zip" placeholder="Zip Code">
-                        </div>
-
-                    </div>
-                    <br><br>
-
-                    <div>
-                        <h4><b> Firm Description & Web Contact Information</b></h4>
-
-                        <div class="input-group card-box">
-                            <span class="details">Email</span>
-                            <input type="email" placeholder="Email" name="femail">
-                        </div>
-                        <div class="input-group card-box">
-                            <span class="details">Web Address - http://</span>
-                            <input type="text" placeholder="Web Address" name="webadd">
-                        </div>
-                        <div class="input-group card-box">
-                            <span class="details">Categories you work in </span>
-                            <textarea id="list" name="list" rows="8" cols="80" placeholder="List out"></textarea>
-                        </div>
-
-                        <div class="input-group card-box">
-                            <span class="details">3 Descriptive lines about your Firm or Services</span>
-                            <textarea id="desc" name="description" rows="8" cols="80" placeholder="Description"></textarea>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <div class="container-contact1-form-btn"><button  type="submit" class="contact1-form-btn">Sign Up</button>
-                <!-- <input type="Submit"> -->
-            </div>
-        </div>   
+      <div class="col-md-6 col-sm-10 col-xs-1 text-center" id="help5">
+        <h2 class="moc">Welcome to ABHAYADA</h2>
+        <p class="lead">Feel safe to register your complaints ANONYMOUSLY!</p>
+        <a href="ab\complaint.php" class="btn-complaint" style="text-decoration: none;">Register Complaint</a>
+        <br>
+        <br>
+        <br>
+        <a href="tracking.html" class="btn-complaint" style="text-decoration: none;">Track your complaint status</a>
+      </div>
+      <div class="col-md-5 col-sm-10 order-2 order-md-1 ">
+        <div id="help4">
+          <img src="images\landingpage\lp2.png" id="img1" class="img-fluid">
+        </div>
+      </div>
     </div>
-    <!-- footer -->
-    <?php require 'utilities\_footer.php' ?>
+  </section>
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
-        crossorigin="anonymous"></script>
+  <!-- box -->
+  <div class="icon-boxes position-relative justify-content-center">
+    <div class="container position-relative">
+      <div class="row gy-4 mt-5">
+        <div class="col-xl-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+          <div class="icon-box">
+            <div class="icon">
+              <h5> <i class="fa-solid fa-venus"></i> Anti-sexual harassment</h5>
+              <p>Empower employees with
+                knowledge - Free from the fear of
+                Sexual Harassment!</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-xl-1"></div>
+        <div class="col-xl-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+          <div class="icon-box">
+            <div class="icon">
+              <h5> <i class="fa-solid fa-brain"></i> Knowledge</h5>
+              <p>Partner with us , on your Learning
+                goals to ensure that it is run
+                efficiently and effectively!</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-xl-1"></div>
+        <div class="col-xl-3 col-md-6 aos-init aos-animate" id="help" data-aos="fade-up" data-aos-delay="100">
+          <div class="icon-box">
+            <div class="icon">
+              <h5> <i class="fa-solid fa-handshake"></i> A Helping Hand</h5>
+              <p>Extending a helping hand to the
+                integral gears of your
+                for a smooth and holistic work
+                culture!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <!-- content -->
+  <section class="container">
+    <div class="row align-items-center" id="help1">
+      <div class="col-lg-5 col-md-6 col-sm-6" id="help8">
+        <h3>ABOUT US</h3>
+        <p class="about">Abhayada is a great platform for women to stand up for themselves and encourage others for the
+          same. The complaint system is quite easy and exclusive so don't think twice before registering the complaint.
+          It is the best platform to stay anonymous and get the work done. We act as an unbiased cover between
+          complainee and workplaces. You can share your feelings on the blog page as it is pshycologocially proven that
+          penning down a mishap is way more self-enhancing and provides deep contentment. YOU ARE NOT ALONE IN THIS!
+          This platform also provides an "Institute Sign-up" option for all the institutions to collaborate with us. It
+          will allow us to work with the Workplace Harassment complaints effectively.
+        </p>
+      </div>
+      <div class="col-lg-1"></div>
+      <div class="col-lg-5" id="help9">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner" id="crs">
+            <div class="carousel-item active">
+              <img src="images\landingpage\c1.jpg" class="d-block " alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="images\landingpage\c2.jpg" class="d-block " alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="images\landingpage\c3.jpg" class="d-block " alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="images\landingpage\c4.jpeg" class="d-block " alt="...">
+            </div>
+
+          </div>
+          <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </button>
+        </div>
+      </div>
+
+  </section>
+  <br>
+  <br>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 align-items-center">
+        <img src="images\landingpage\mission.png" id="mv">
+        <span class="ml-3" id="mission">OUR MISSION</span>
+        <br>
+        <br>
+        <p id="help10">To strive towards enabling women to achieve equality and equal participation in the workplace and
+          outside. And thus providing them a convenient way of tackling harassment </p>
+      </div>
+      <div class="col-lg-2"></div>
+      <div class="col-lg-5">
+        <img src="images\landingpage\vision.png" id="mv">
+        <span class="ml-3" id="mission">OUR VISION</span>
+        <br>
+        <BR>
+        <P id="help10">To withhold women's dignity and self-respect irrespective of their social status for a secure and
+          relatively much more ethical workplace.</P>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <br>
+  <div class="card-group container">
+    <div class="container">
+      <h4 id="help11">Collaborating Partners</h4>
+    </div>
+
+    <div class="card">
+      <img src="images\logos\bmw.jpg" class="card-img-top" alt="">
+    </div>
+    <div class="card">
+      <img src="images\logos\patel.jpg" class="card-img-top" alt="">
+    </div>
+    <div class="card">
+      <img src="images\logos\anjali.jpg" class="card-img-top" alt="">
+    </div>
+    <div class="card">
+      <img src="images\logos\om.jpg" class="card-img-top" alt="">
+    </div>
+    <div class="card">
+      <img src="images\logos\indianoil.jpg" class="card-img-top" alt="">
+    </div>
+  </div>
+  <!-- footer -->
+  <?php require 'utilities\_footer.php' ?>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+    crossorigin="anonymous"></script>
+  <script>
+    $(function () {
+      $(document).scroll(function () {
+        var $nav = $("#mainnav");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+      });
+    });
+  </script>
 </body>
 
 </html>
