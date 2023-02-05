e030325a4232112eb735cad286cc2322de7375eb
Anjali Bohra <anjali.bohra2111@gmail.com>
2023-02-05 18:29:39 +0530

    Update lawyer.php

--- lawyer.php
+++ lawyer.php
@@ -40,7 +40,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="C:\Users\Arpana\Desktop\css\sign3.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
