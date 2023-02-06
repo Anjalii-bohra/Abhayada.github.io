5020017b41b3cf11f314098311cedceec1de2839
Khushikshatriya28 <khushikshatriya2806@gmail.com>
2023-02-07 00:44:09 +0800

    Update complaint.php

--- complaint.php
+++ complaint.php
@@ -29,10 +29,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="complaint.css">
+    <link href="complaint.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
     <meta name="robots" content="noindex, follow">
 </head>
 
@@ -141,38 +138,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
             </div>
 
         </div>
-        <!-- <div class="box">
-            your id is: <span id="otp"> </span>
 
-        </div>
-
-        <button type="button" onclick="generate()"> generate id</button>
-
-        <script>
-            function generate() {
-                var num = '1234567890';
-                let OTP = '';
-
-                for (let i = 0; i < 4; i++) {
-
-                    OTP += num[Math.floor(Math.random() * 10)];
-
-                }
-
-                document.getElementById('otp').innerHTML = OTP;
-            }
-        </script> -->
     </div>
 <!-- footer  -->
 <?php require 'utilities\_footer.php' ?> 
 
 <script src="tilt.jquery.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+        crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
-        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
