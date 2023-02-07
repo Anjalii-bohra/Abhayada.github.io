082bf632be6d89be4be073937bba08fbe19910ca
Anjali Bohra <anjali.bohra2111@gmail.com>
2023-02-08 02:51:04 +0530

    Update _footer.php

--- utilities/_footer.php
+++ utilities/_footer.php
@@ -1,49 +1,5 @@
 <footer id="footer" class="footer">
 
-  <div class="container">
-    <div class="row gy-4">
-      <div class="col-lg-6 col-md-6 col-sm-12 footer-info">
-        <a href="index.php" class="logo d-flex align-items-center" id="abhay">
-          <span style="font-size: 1.7em;"><i style="color: black;">Abhayada</i></span>
-        </a>
-        <p id="fpara">It's easy to ignore when it's happening to others and almost easier to ignore when happening to
-          you because it is something that you don't think that you are big enough, you don't think you are strong
-          enough but the experience shared by others solidify your courage to act against sexual harassment.</p>
-      </div>
-      <div class="col-lg-1 col-md-2"></div>
-      <div class="col-lg-2 col-md-4  footer-links">
-        <h4 id="uselink">Useful Links</h4>
-        <ul>
-          <li>
-            <a href="index.php" style="color: black;">Home</a>
-          </li>
-          <li>
-            <a href="laws.php" style="color: black;">Laws and Awareness</a>
-          </li>
-          <li>
-            <a href="legal-advice.php" style="color: black;">Legal Consultancy</a>
-          </li>
-          <li>
-            <a href="#" style="color: black;">About Us</a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-lg-1 col-md-2"></div>
-      <div class="col-lg-2 col-md-12 footer-contact text-center text-md-start">
-        <h4 id="cus">Contact Us</h4>
-        <p id="contactinfo">
-          <strong>Phone: </strong>9979999900
-          <br>
-          <strong>Email: </strong>abc@gmail.com
-          <br>
-        </p>
-        <div class="social-links d-flex ml-3" id="socio">
-          <a href="#" class="logos" style="color:black;"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
-              class="bi bi-twitter" viewBox="0 0 16 16">
-              <path
-                d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-            </svg></a>
-
     <div class="container">
         <div class="row gy-4">
             <div class="col-lg-6 col-md-6 col-sm-12 footer-info">
