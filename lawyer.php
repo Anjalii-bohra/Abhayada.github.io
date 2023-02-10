5684774b111a9dd4dbbaf066532bd00ffcdc65d9
Anjali Bohra <anjali.bohra2111@gmail.com>
2023-02-11 02:26:04 +0530

    Update lawyer.php

--- lawyer.php
+++ lawyer.php
@@ -1,26 +1,25 @@
 <?php
 $insert = false;
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include 'utilities\_dbconnect.php';
     $pname = $_POST["name"];
     $email = $_POST["email"];
     $contact = $_POST["contact"];
     $address = $_POST["address"];
-    $city= $_POST["city"];
-    $state= $_POST["state"];
-    $zip= $_POST["zip"];
-    $femail= $_POST["femail"];
-    $webaddress= $_POST["webadd"];
-    $list= $_POST["list"];
-    $description= $_POST["description"];
+    $city = $_POST["city"];
+    $state = $_POST["state"];
+    $zip = $_POST["zip"];
+    $femail = $_POST["femail"];
+    $webaddress = $_POST["webadd"];
+    $list = $_POST["list"];
+    $description = $_POST["description"];
 
     //sql query
     $sql = "INSERT INTO `lawyers` ( `full-name`, `email`, `contact`, `address`, `city`, `state`, `zipcode`, `firm-email`, `web-address`, `category`, `description`, `date`) VALUES ('$pname','$email','$contact','$address','$city','$state','$zip',  '$femail', '$webaddress', '$list', '$description', current_timestamp());";
 
     $result = mysqli_query($conn, $sql);
     //checking
-    if($result){
+    if ($result) {
         $insert = true;
     }
 }
@@ -34,15 +33,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-        <link href="lawyer.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+    <link href="lawyer.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
     <title>Lawyer-form</title>
 </head>
 
 <body>
-        <!-- NAV-BAR -->
-        <?php require 'utilities\navbar.php' ?> 
+    <!-- NAV-BAR -->
+    <?php require 'utilities\navbar.php' ?>
     <?php
-    if($insert){
+    if ($insert) {
         echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
         <strong>Success!</strong> 
         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
@@ -64,12 +63,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
                         difference, and feel good about yourself.
                     </p>
                     <div class="a2"><b>If you want voluntarily sign-up and provide legal support/advice to our
-                        victims </b></div>
+                            victims </b></div>
 
                     <div class="mt-4">
                         <h4><b>About Yourself: </b> </h4>
                         <div class="input-group card-box">
-                            <p style="font-size: 19px;" name="y/n">Are you comfortable initially communicating with a client by answering a question via email ?</p>
+                            <p style="font-size: 19px;" name="y/n">Are you comfortable initially communicating with a
+                                client by answering a question via email ?</p>
                             <div class="form-check form-check-inline" id="nnnn">
                                 <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
                                     value="option1">
@@ -80,6 +80,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
                                     value="option2">
                                 <label class="form-check-label" for="inlineRadio2">NO</label>
                             </div>
+                            <?php
+                            if (isset($_POST['y/n'])) {
+                                $selected_color = $_POST['y/n'];
+                                // Store the selected value in a database or do some other processing with it
+                            }
+                            ?>
+
                         </div>
 
                         <div class="input-group card-box">
@@ -138,18 +145,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 
                         <div class="input-group card-box">
                             <span class="details">3 Descriptive lines about your Firm or Services</span>
-                            <textarea id="desc" name="description" rows="8" cols="80" placeholder="Description"></textarea>
+                            <textarea id="desc" name="description" rows="8" cols="80"
+                                placeholder="Description"></textarea>
                         </div>
                     </div>
                 </div>
                 <div class="container-contact1-form-btn">
-                <button  type="submit" class="contact1-form-btn">
-                    Sign Up</button>
-                <!-- <input type="Submit"> -->
-            </div>
+                    <button type="submit" class="contact1-form-btn">
+                        Sign Up</button>
+                    <!-- <input type="Submit"> -->
+                </div>
             </form>
-            
-        </div>   
+
+        </div>
     </div>
     <!-- footer -->
     <?php require 'utilities\_footer.php' ?>
